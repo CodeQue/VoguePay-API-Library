b2bc3eee49a0da3eef9dd886fcf7792a92318b0f
CodeQue <ojikutu@Abdul-Khaliqs-MacBook-Pro.local>
2019-05-02 10:56:40 +0100

    Bug Fix

--- samples/card.php
+++ samples/card.php
@@ -42,15 +42,15 @@ $data = [
     ],
     "demo" => "false", // boolean (true / false) , set to true to imitate a demo transaction and false for live transaction
 ];
-    print_r(voguepay::card($data)); 
-    /*
-    stdClass Object
-    (
-        [description] => Redirection Required - 3D Authentication required. // Response code description
-        [redirectUrl] => https://voguepay.com/?p=vpgate&ref=czoxMzoiNWNiZjQ2OTBlNDFkMCI7 // 3D redirection URL
-        [reference] => 1x2345vbn // Transaction reference
-        [response] => WL3D // Transaction response
-        [status] => OK // API query status
-        [transactionID] => 5cbf4690e41d0 // Generated VoguePay transaction ID
-    )
-    */
+print_r(voguepay::card($data)); 
+/*
+stdClass Object
+(
+    [description] => Redirection Required - 3D Authentication required. // Response code description
+    [redirectUrl] => https://voguepay.com/?p=vpgate&ref=czoxMzoiNWNiZjQ2OTBlNDFkMCI7 // 3D redirection URL
+    [reference] => 1x2345vbn // Transaction reference
+    [response] => WL3D // Transaction response
+    [status] => OK // API query status
+    [transactionID] => 5cbf4690e41d0 // Generated VoguePay transaction ID
+)
+*/
